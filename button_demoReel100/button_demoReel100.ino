f8e8a17b3f5af16fa1b32955db828c0bfca72da2
Andrew Tuline <atuline@gmail.com>
2017-02-12 16:04:53 -0800

    Replace button library.

--- button_demoReel100/button_demoReel100.ino
+++ button_demoReel100/button_demoReel100.ino
@@ -12,8 +12,10 @@
  * Instructions:
  * 
  * Program reads display mode from EEPROM and displays it.
- * Push button to change to the next mode.
- * Push button for > 1 second to write the current mode to EEPROM.
+ * Click to change to the next mode.
+ * Hold button for > 1 second to write the current mode to EEPROM.
+ * Double click to reset to mode 0.
+ * 
  * There's also debug output to the serial monitor . . just to be sure.
  * 
  * Requirements:
@@ -24,7 +26,9 @@
  * 
  */
 
-#include "Button.h"                                           // https://github.com/JChristensen/Button
+#define buttonPin 6                                           // input pin to use as a digital input
+#include "jsbutton.h"                                         // Nice button routine by Jeff Saltzman
+
 #include "EEPROM.h"
 #include "FastLED.h"
 
@@ -41,28 +45,17 @@
 #define NUM_LEDS 60                                           // Number of LED's.
 
 
-#define BUTTON_PIN 6                                          // Connect a buttonor something similar from Arduino pin 6 to ground.
-#define PULLUP true                                           // To keep things simple, we use the Arduino's internal pullup resistor.
-#define INVERT true                                           // Since the pullup resistor will keep the pin high unless the
-                                                              // switch is closed, this is negative logic, i.e. a high state
-                                                              // means the button is NOT pressed. (Assuming a normally open switch.)
-#define DEBOUNCE_MS 50                                        // A debounce time of 50 milliseconds seems to work well.
 
-
-// Button variables
-Button myBtn(BUTTON_PIN, PULLUP, INVERT, DEBOUNCE_MS);        //Declare the button
-boolean longpress;
 
 // Definition for the array of routines to display.
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
 //Mode and EEPROM variables
-//uint8_t ledMode = 0;
-//uint8_t maxMode = 6;
+uint8_t maxMode = 5;                                          // Maximum number of display modes. Would prefer to get this another way, but whatever.
 int eepaddress = 0;
 
 // Global variables can be changed on the fly.
-uint8_t max_bright = 128;                                      // Overall brightness definition. It can be changed on the fly.
+uint8_t max_bright = 128;                                     // Overall brightness definition. It can be changed on the fly.
 
 struct CRGB leds[NUM_LEDS];                                   // Initialize our LED array.
 
@@ -77,6 +70,9 @@ void setup() {
 
   Serial.begin(57600);                                        // Initialize serial port for debugging.
   delay(1000);                                                // Soft startup to ease the flow of electrons.
+
+  pinMode(buttonPin, INPUT);                                  // Set button input pin
+  digitalWrite(buttonPin, HIGH );
   
   LEDS.addLeds<LED_TYPE, LED_DT, LED_CK, COLOR_ORDER>(leds, NUM_LEDS);  // Use this for WS2801 or APA102
 //  LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);  // Use this for WS2812
@@ -85,6 +81,10 @@ void setup() {
   set_max_power_in_volts_and_milliamps(5, 500);               // FastLED Power management set at 5V, 500mA.
 
   gCurrentPatternNumber = EEPROM.read(eepaddress);
+
+  if (gCurrentPatternNumber > maxMode) gCurrentPatternNumber = 0;   // A safety in case the EEPROM has an illegal value.
+
+  
   Serial.print("Starting ledMode: ");
   Serial.println(gCurrentPatternNumber);
 
@@ -92,19 +92,19 @@ void setup() {
 
 
 
-SimplePatternList gPatterns = {rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+SimplePatternList gPatterns = {rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };               // Don't know why this has to be here. . .
 
  
 
 void loop() {
 
   readbutton();
-
+  
   EVERY_N_MILLISECONDS(50) {
-    gPatterns[gCurrentPatternNumber]();                         // Call the current pattern function once, updating the 'leds' array    
+    gPatterns[gCurrentPatternNumber]();                       // Call the current pattern function once, updating the 'leds' array    
   }
 
-  EVERY_N_MILLISECONDS(20) {                                    // slowly cycle the "base color" through the rainbow
+  EVERY_N_MILLISECONDS(20) {                                  // slowly cycle the "base color" through the rainbow
     gHue++;
   }
 
@@ -114,23 +114,24 @@ void loop() {
 
 
 
-void readbutton() {                                        // Read the button and increase the mode
-  
-  myBtn.read();
+void readbutton() {                                           // Read the button and increase the mode
 
-  if(myBtn.wasReleased()) {
-    if (longpress==1) {
-      EEPROM.write(eepaddress,gCurrentPatternNumber);
-      Serial.print("Writing: ");
-    } else {
-      gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE(gPatterns);
-    }
-    longpress = 0;
+  uint8_t b = checkButton();
+
+  if (b == 1) {                                               // Just a click event to advance to next pattern
+    gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE(gPatterns);
     Serial.println(gCurrentPatternNumber);
   }
-  
-  if(myBtn.pressedFor(1000)) {
-    longpress = 1;
+
+  if (b == 2) {                                               // A double-click event to reset to 0 pattern
+    gCurrentPatternNumber = 0;
+    Serial.println(gCurrentPatternNumber);
+  }
+
+  if (b == 3) {                                               // A hold event to write current pattern to EEPROM
+    EEPROM.write(eepaddress,gCurrentPatternNumber);
+    Serial.print("Writing: ");
+    Serial.println(gCurrentPatternNumber);    
   }
 
 } // readbutton()
@@ -142,7 +143,7 @@ void readbutton() {                                        // Read the button an
 
 void rainbow() {
 
-  fill_rainbow(leds, NUM_LEDS, gHue, 7);                       // FastLED's built-in rainbow generator.
+  fill_rainbow(leds, NUM_LEDS, gHue, 7);                      // FastLED's built-in rainbow generator.
   
 } // rainbow()
 
@@ -150,7 +151,7 @@ void rainbow() {
 
 void rainbowWithGlitter() {
 
-  rainbow();                                                    // Built-in FastLED rainbow, plus some random sparkly glitter.
+  rainbow();                                                  // Built-in FastLED rainbow, plus some random sparkly glitter.
   addGlitter(80);
   
 } // rainbowWithGlitter()
@@ -187,7 +188,7 @@ void sinelon() {                                              // A colored dot s
 
 
 
-void bpm() {                                                // Colored stripes pulsing at a defined Beats-Per-Minute.
+void bpm() {                                                  // Colored stripes pulsing at a defined Beats-Per-Minute.
 
   uint8_t BeatsPerMinute = 62;
   CRGBPalette16 palette = PartyColors_p;
@@ -201,7 +202,7 @@ void bpm() {                                                // Colored stripes p
 
 
 
-void juggle() {                                             // Eight colored dots, weaving in and out of sync with each other.
+void juggle() {                                               // Eight colored dots, weaving in and out of sync with each other.
 
   fadeToBlackBy(leds, NUM_LEDS, 20);
   byte dothue = 0;
