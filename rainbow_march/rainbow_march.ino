3d4e27657248c81ca4daab575b75b420466331e6
Andrew Tuline <atuline@gmail.com>
2018-09-03 12:41:16 -0700

    Update rainbow_march.

--- rainbow_march/rainbow_march.ino
+++ rainbow_march/rainbow_march.ino
@@ -1,11 +1,23 @@
 
-/* rainbow_march for FastLED
+/* rainbow_march
 
 By: Andrew Tuline
 
 Date: Nov, 2014
 
+Updated: June, 2018
+
+Update:
+
+1.1 Remove original delay and added EVERY_N_MILLISECONDS
+1.2 Remove EVERY_N_MILLISECONDS and use millis() for timing. Also demonstrate beat8 and beatsin8.
+
+
+Description
+
 Rainbow marching up the strand. Very basic, but oh so popular. Oh look, we don't need to add a 'wheel' routine.
+
+If you want something really funky, try generating sequences with palettes, especially palettes you can change on the flly.
 
 */
 
@@ -21,40 +33,45 @@ Rainbow marching up the strand. Very basic, but oh so popular. Oh look, we don't
 #define LED_CK 11                                             // Clock pin for WS2801 or APA102
 #define COLOR_ORDER BGR                                       // It's GRB for WS2812B and GBR for APA102
 #define LED_TYPE APA102                                       // What kind of strip are you using (APA102, WS2801 or WS2812B)?
-#define NUM_LEDS 20                                           // Number of LED's
+#define NUM_LEDS 60                                           // Number of LED's
 
 // Initialize changeable global variables.
-uint8_t max_bright = 128;                                     // Overall brightness definition. It can be changed on the fly.
+uint8_t max_bright = 255;                                     // Overall brightness definition. It can be changed on the fly.
 
 struct CRGB leds[NUM_LEDS];                                   // Initialize our LED array.
 
 
-// Initialize global variables for sequences
-uint8_t thisdelay = 5;                                        // A delay value for the sequence(s)
-uint8_t thishue = 0;                                          // Starting hue value.
-uint8_t deltahue = 10;                                        // Hue change between pixels.
-
 
 void setup() {
+
   Serial.begin(57600);
 
 //  LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);         // For WS2812B
   LEDS.addLeds<LED_TYPE, LED_DT, LED_CK, COLOR_ORDER>(leds, NUM_LEDS);   // For APA102 or WS2801
 
   FastLED.setBrightness(max_bright);
-  set_max_power_in_volts_and_milliamps(5, 500);               // FastLED 2.1 Power management set at 5V, 500mA
+  set_max_power_in_volts_and_milliamps(5, 1000);              // FastLED 2.1 Power management set at 5V, 500mA
+
 } // setup()
 
 
+
 void loop () {
-  EVERY_N_MILLISECONDS(thisdelay) {                           // FastLED based non-blocking routine to update/display the sequence.
-    rainbow_march();
-  }
-  show_at_max_brightness_for_power();  
+
+  rainbow_march(200, 10);
+  FastLED.show();
+  
 } // loop()
 
 
-void rainbow_march() {                                        // The fill_rainbow call doesn't support brightness levels
-  thishue++;                                                  // Increment the starting hue.
+
+void rainbow_march(uint8_t thisdelay, uint8_t deltahue) {     // The fill_rainbow call doesn't support brightness levels.
+
+  uint8_t thishue = millis()*(255-thisdelay)/255;             // To change the rate, add a beat or something to the result. 'thisdelay' must be a fixed value.
+  
+// thishue = beat8(50);                                       // This uses a FastLED sawtooth generator. Again, the '50' should not change on the fly.
+// thishue = beatsin8(50,0,255);                              // This can change speeds on the fly. You can also add these to each other.
+  
   fill_rainbow(leds, NUM_LEDS, thishue, deltahue);            // Use FastLED's fill_rainbow routine.
+
 } // rainbow_march()
